557b730de11d37907c790d0f292edb7ffa249066
xtrp <36894883+xtrp@users.noreply.github.com>
2020-02-21 21:30:48 +0000

    finish schoology overdue box remover extension with icon

--- popup.js
+++ popup.js
@@ -1,1 +1,27 @@
-alert("test");
+let checkOverdueBoxContentLoop;
+
+// only run script if on Schoology homepage
+if(window.location.pathname == "/home") {
+	// overdue box HTML elm
+	const overdueBoxElm = document.querySelector(".overdue-submissions.overdue-submissions-wrapper");
+
+	// content loop, check if overdue box has content and acts accordingly
+	checkOverdueBoxContentLoop = setInterval(() => {
+		const textOfOverdueBox = overdueBoxElm.innerText;
+		
+		// if overdue box has content, remove and end check loop
+		if(textOfOverdueBox.length > 0) {
+			// delete the overdue box!
+			overdueBoxElm.innerHTML = "<p style='text-align: center; font-weight: bold; font-size: 1.75rem;'>Overdue Box Removed! 🤷‍♂️</p>"; // tried removing the elm with DOM nodes, didn't work so used outerHTML instead
+			overdueBoxElm.style.backgroundColor = "#e74c3c";
+			overdueBoxElm.style.color = "#ffffff";
+			overdueBoxElm.style.padding = "1rem";
+			overdueBoxElm.style.opacity = ".5";
+
+			// end check content loop (interval)
+			clearInterval(checkOverdueBoxContentLoop);
+		}
+	}, 16);
+}else {
+	checkOverdueBoxContentLoop = null;
+}
